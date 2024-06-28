866e6318188d19af2cee35b2eb113a0a2ba134b8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-28 22:54:28 +0100

    Updated App.test.tsx

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,18 @@
-import { render } from '@testing-library/react';
-import { describe, it} from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect} from 'vitest';
 import App from '../App';
 
 describe('renders App components correctly', () => {
     it('renders without crashing', () => {
       render(<App />);
+      
+      const headerComponent = screen.findByRole('header');
+      expect(headerComponent).toBeVisible();
+  
+      const mainComponent = screen.findByRole('main');
+      expect(mainComponent).toBeVisible();
+  
+      const footerComponent = screen.findByRole('footer');
+      expect(footerComponent).toBeVisible();
     });
 });
