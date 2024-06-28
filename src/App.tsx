3ac09077f26d6330aa67cef8eecc8068f4898d3e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-28 22:59:53 +0100

    Updated App.tsx to pass the test

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@
 const App = () => {
 
   return (
-    <>
+    <div className="App">
+      <div role="header">
         <h1>World Minds</h1>
-    </>
+      </div>
+      <div role="main">
+        <p>main</p>
+      </div>
+      <div role="footer">
+        <p>footer</p>
+      </div>
+    </div>
   )
 }
 
