ea99a2930f954a3ef3bd84209aa8158e4f68b8f2
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-29 13:08:43 +0100

    Created FooterHelper.tsx

---
+++ src/assets/FooterHelpers.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HelpersWrapper = styled.div`
+  padding: 1em;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  color: #f0ece2; 
+  background: #596e79;
+  max-width: 900px;
+  margin: 0 auto;
+`;
+
+const ContactContainer = styled.ul`
+  list-style: none;
+  text-align: center;
+`;
+
+const ContactLi = styled.li`
+  padding: 0.2em 0;
+  transition: 200ms all ease;
+  cursor: pointer;
+
+  &:hover {
+    color: black; 
+  }
+`;
+
+const HelpersContainer = styled.ul`
+  padding: 1em;
+  list-style: none;
+  text-align: center;
+`;
+
+const ListLi = styled.li`
+  color: #f0ece2;
+  transition: 200ms all ease;
+  cursor: pointer;
+
+  &:hover {
+    color: black; 
+  }
+`;
+
+const FooterHelpers: React.FC = () => {
+  return (
+    <HelpersWrapper>
+      <ContactContainer data-testid='contactContainer'>
+        <ContactLi role='contactLink'>Contact us</ContactLi>
+        <ContactLi data-testid='phoneNumber'>+1-543-123-4567</ContactLi>
+        <ContactLi data-testid='email'>example@worldminds.com</ContactLi>
+      </ContactContainer>
+      <HelpersContainer data-testid='helpersContainer'>
+        <ListLi role='homeLink'>About Us</ListLi>
+        <ListLi role='faqLink'>FAQ</ListLi>
+        <ListLi role='careerLink'>Career</ListLi>
+        <ListLi role='blogLink'>Blog</ListLi>
+        <ListLi role='supportLink'>Support</ListLi>
+      </HelpersContainer>
+    </HelpersWrapper>
+  )
+}
+
+export default FooterHelpers
