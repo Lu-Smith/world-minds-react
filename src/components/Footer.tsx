bdde991ac507a7b8a493cc93a08f5232c64bbf76
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-29 13:09:31 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import SocialMedia from '../assets/SocialMedia';
+import FooterLink from '../assets/FooterLink';
+import FooterHelpers from '../assets/FooterHelpers';
 
-const Footer = () => {
+const FooterText = styled.h4`
+  font-size: 14px;
+  padding: 1em  0;
+  color: #596e79; 
+  font-family: "Playwrite US Modern", cursive;
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-width: 768px) {
+    font-size: 10px;
+  }
+`;
+
+const Footer: React.FC = () => {
   return (
-    <div>Footer</div>
+    <>
+      <SocialMedia />
+      <FooterHelpers />
+      <FooterText>
+        Coded by
+        <FooterLink href='https://www.lunasmithart.com' target='_blank'>Luna Smith</FooterLink>
+        , open-source on
+        <FooterLink href='https://github.com/Lu-Smith/world-minds-react' target='_blank'>GitHub</FooterLink>.
+      </FooterText>
+    </>
   )
 }
 
