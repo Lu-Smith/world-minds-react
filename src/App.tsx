7baafc3e904a7bf74d86855318e475628508635b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-29 14:32:12 +0100

    Updated App.tsx  to pass tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import Main from "./components/Main"
+import React from 'react';
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+
 
 const App: React.FC = () => {
 
   return (
     <div className="App">
-      <div role="bannerContainer">
+      <div role="headerContainer">
         <Header />
+      </div>
+      <div role="bannerContainer">
+        <Banner />
       </div>
       <div role="mainContainer">
         <Main />
