f7cb07dbbcaf98ded7864d7287e5427c3a579d7e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-29 22:59:20 +0100

    Updated FooterLink.tsx

--- src/assets/FooterLink.tsx
+++ src/assets/FooterLink.tsx
@@ -3,13 +3,15 @@ import styled from 'styled-components';
 import { motion } from "framer-motion";
 
 const FooterLinkElement = styled.a`
-  color: #596e79; 
+  color: #2b2b91;
   padding: 0.4em 0.5em;
   text-decoration: none;
   transition: 200ms all ease;
+  cursor: pointer;
+  font-size: 1.2em;
 
   &:hover {
-    color: black; 
+    color: #781E36;
   }
 `;
 
@@ -23,8 +25,8 @@ interface FooterLinkProps {
 const FooterLink: React.FC<FooterLinkProps> = ({ href, target, children, role }) => {
   return (
     <motion.div
-    initial={{ scale: 1, opacity: 1, y: 0 }}
-    whileHover={{ scale: 1.2, opacity: 0.8 }}
+    initial={{ scale: 1, y: 0, opacity: 1 }}
+    whileHover={{ scale: 1.2, y: -15, opacity: 0.9 }}
     transition={{ duration: 0.6 }}>
       <FooterLinkElement href={href} target={target} role={role}>
         {children}
