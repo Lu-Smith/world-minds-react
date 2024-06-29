392bee3f64409f661c5d132d726a8f56441c52ea
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-29 14:58:16 +0100

    Updated Banner.tsx to pass tests

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+import BannerVideo from '../assets/media/banner.mp4';
+
+const BannerImageContainer = styled.div`
+  background-color: black;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const BannerImage = styled.video`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
 
 const Banner: React.FC = () => {
   return (
-    <div>Banner</div>
+    <BannerImageContainer role='bannerImageContainer'>
+      <BannerImage src={BannerVideo} autoPlay loop muted />
+    </BannerImageContainer>
   )
 }
 
