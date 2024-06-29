52948d1277ed4cd4b94412f9e17ff5b74505bb1c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-29 13:09:16 +0100

    Created SocialMedia.tsx

---
+++ src/assets/SocialMedia.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+    FaInstagram,
+    FaFacebook,
+    FaTwitter,
+  } from "react-icons/fa";
+import FooterLink from './FooterLink';
+
+const SocialMediaContainer = styled.div`
+  color: #596e79;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 40px;
+  font-size: 20px;
+  margin: 10px 0;
+`;
+
+const SocialMedia: React.FC = () => {
+  return (
+    <SocialMediaContainer>
+      <FooterLink href="//www.facebook.com/lunasmithart" target="_blank" aria-label="Facebook page" role="facebook" >
+        <FaFacebook />
+      </FooterLink>
+      <FooterLink href="//twitter.com/LuArtGallery" target="_blank" aria-label="Twitter page" role="twitter" >
+        <FaTwitter />
+      </FooterLink>
+      <FooterLink href="//www.instagram.com/luartgallery" target="_blank" aria-label="Instagram page" role="instagram" >
+        <FaInstagram />
+      </FooterLink>
+    </SocialMediaContainer>
+  )
+}
+
+export default SocialMedia
