4c6655aabee60f63cc9efea127b43d9db06c19cb
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-29 14:57:55 +0100

    Created Banner.test.tsx

---
+++ src/tests/Banner.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect} from 'vitest';
+import Banner from '../components/Banner';
+
+describe('renders Banner components correctly', () => {
+    it('renders without crashing', () => {
+      render(<Banner />);
+      
+      const bannerImageContainer = screen.getByRole('bannerImageContainer');
+      expect(bannerImageContainer).toBeVisible();
+
+      const bannerImage = bannerImageContainer.querySelector('video');
+      expect(bannerImage).toBeInTheDocument();
+      expect(bannerImage).toHaveAttribute('src', expect.stringContaining('banner.mp4'));
+      expect(bannerImage).toHaveAttribute('autoPlay');
+      expect(bannerImage).toHaveAttribute('loop');
+
+    });
+});
