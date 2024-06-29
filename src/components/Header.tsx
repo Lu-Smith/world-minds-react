71222057ec03e3bcb570afa1171ebc81ce3534f1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-29 13:46:23 +0100

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Button from '../assets/Button';
+import { BiWorld } from "react-icons/bi";
+
+const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  font-size: 1.8em;
+  color: black; 
+  text-align: left;
+  font-family: "Playwrite US Modern", cursive;
+  letter-spacing: 1px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Header: React.FC = () => {
   return (
     <div>
-        <h1>World Minds</h1>
+      <LogoContainer role='logoContainer'>
+        <Title data-testid='logo'>
+          W<div><BiWorld /></div>orld Minds
+        </Title>
+      </LogoContainer>
+      <Button>Button</Button>
     </div>
   )
 }
