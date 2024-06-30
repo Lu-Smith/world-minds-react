8bc28187b9bd54f6394d9445d9a94769d9c860f8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-30 19:43:24 +0100

    Updated Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import BannerVideo from '../assets/media/banner.mp4';
+import Button from '../assets/Button';
 
 const BannerImageContainer = styled.div`
   background-color: black;
@@ -14,10 +15,35 @@ const BannerImage = styled.video`
   object-fit: cover;
 `;
 
+const BannerContentContainer = styled.div`
+  position: relative;
+  top: -55%;
+`;
+
+const BannerTitle = styled.h1`
+  font-size: 1.8em;
+  font-family: "Playwrite US Modern", cursive;
+  letter-spacing: 1px;
+`;
+
+const BannerSubTitle = styled.h2`
+  font-size: 1.2em;
+  letter-spacing: 1px;
+  font-style: italic;
+  font-weight: 300;
+  margin-bottom: 20px;
+`;
+
+
 const Banner: React.FC = () => {
   return (
     <BannerImageContainer role='bannerImageContainer'>
       <BannerImage src={BannerVideo} autoPlay loop muted />
+      <BannerContentContainer>
+        <BannerTitle>Welcome to World Minds</BannerTitle>
+        <BannerSubTitle>Open your mind and find yourself</BannerSubTitle>
+        <Button role='join'>Join</Button>
+      </BannerContentContainer>
     </BannerImageContainer>
   )
 }
