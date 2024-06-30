278ae9784cdc07d097445a11785ff54b90c0ea9d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-30 19:43:07 +0100

    Updated Banner.test.tsx

--- src/tests/Banner.test.tsx
+++ src/tests/Banner.test.tsx
@@ -15,4 +15,43 @@ describe('renders Banner components correctly', () => {
       expect(bannerImage).toHaveAttribute('autoPlay');
       expect(bannerImage).toHaveAttribute('loop');
     });
+
+    it('renders the BannerContentContainer', () => {
+      render(<Banner />);
+  
+      const bannerContentContainer = screen.getByRole('bannerImageContainer').querySelector('div');
+      expect(bannerContentContainer).toBeInTheDocument();
+      expect(bannerContentContainer).toHaveStyle('position: relative');
+      expect(bannerContentContainer).toHaveStyle('top: -55%');
+    });
+
+    it('renders the BannerTitle', () => {
+      render(<Banner />);
+  
+      const bannerTitle = screen.getByText('Welcome to World Minds');
+      expect(bannerTitle).toBeInTheDocument();
+      expect(bannerTitle.tagName).toBe('H1');
+      expect(bannerTitle).toHaveStyle('font-size: 1.8em');
+    });
+
+    it('renders the BannerSubTitle', () => {
+      render(<Banner />);
+  
+      const bannerSubTitle = screen.getByText('Open your mind and find yourself');
+      expect(bannerSubTitle).toBeInTheDocument();
+      expect(bannerSubTitle.tagName).toBe('H2');
+      expect(bannerSubTitle).toHaveStyle('font-size: 1.2em');
+      expect(bannerSubTitle).toHaveStyle('font-style: italic');
+      expect(bannerSubTitle).toHaveStyle('font-weight: 300');
+      expect(bannerSubTitle).toHaveStyle('margin-bottom: 20px');
+    });
+  
+    it('renders the Button', () => {
+      render(<Banner />);
+  
+      const joinButton = screen.getByRole('join');
+      expect(joinButton).toBeInTheDocument();
+      expect(joinButton.tagName).toBe('BUTTON'); 
+    });
+  
 });
