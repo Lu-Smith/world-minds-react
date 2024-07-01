42a3762694edecc5862fa0d8b3ca5d9030e14170
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-07-01 17:33:00 +0100

    Updated css in Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,6 +7,17 @@ const BannerImageContainer = styled.div`
   background-color: black;
   width: 100vw;
   height: 100vh;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5); 
+    pointer-events: none;
+  }
 `;
 
 const BannerImage = styled.video`
