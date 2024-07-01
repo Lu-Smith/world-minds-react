420508522f2478cb32cb47a2aaf2f66b1c43f871
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-07-01 17:31:20 +0100

    Created mainContent.ts

---
+++ src/assets/mainContent.ts
@@ -0,0 +1,54 @@
+import Minds from './media/undraw_game_world_re_e44j.svg';
+import World from './media/undraw_coffee_with_friends_3cbj.svg';
+import Link from './media/undraw_share_link_re_54rx.svg';
+import Collaborate from './media/undraw_design_team_re_gh2d.svg';
+import Ideas from './media/undraw_ideas_flow_re_bmea.svg';
+import Share from './media/undraw_ideas_re_7twj.svg';
+
+
+const mainContent = [
+    {
+        title: 'INVEST IN MINDS',
+        subtitle: 'Empower Future Innovators',
+        content: 'Join us in nurturing the next generation of thinkers, creators, and leaders. By investing in minds, you support education, innovation, and progress. Help us build a world where potential is realized and ideas come to life.',
+        button: 'Get in touch',
+        src: Minds,
+    },
+    {
+        title: 'EXPLORE NEW IDEAS',
+        subtitle: 'Unlock Creativity',
+        content: 'Dive into a world of creativity and innovation. Our platform provides resources and opportunities for individuals to explore new ideas and transform them into reality. Together, we can push the boundaries of what’s possible.',
+        button: 'Learn More',
+        src: World,
+    },
+    {
+        title: 'TECHNOLOGY ADVANCEMENT',
+        subtitle: 'Driving Digital Transformation',
+        content: 'Stay ahead in the rapidly evolving tech landscape. We provide insights and resources to help you navigate and leverage emerging technologies. Empower your projects with the latest advancements in tech.',
+        button: 'Explore Tech',
+        src: Link,
+    },
+    {
+        title: 'PERSONAL GROWTH',
+        subtitle: 'Unlock Your Potential',
+        content: 'Investing in personal growth is crucial for achieving success. Our programs and resources are designed to help you develop new skills, enhance your capabilities, and reach your full potential.',
+        button: 'Start Growing',
+        src: Collaborate,
+    },
+    {
+        title: 'COLLABORATE AND GROW',
+        subtitle: 'Build Strong Networks',
+        content: 'Collaboration is key to growth and success. Connect with like-minded individuals and organizations to share knowledge, resources, and support. Join a community that fosters teamwork and collective advancement.',
+        button: 'Join Now',
+        src: Ideas,
+    },
+    {
+        title: 'SUSTAINABLE FUTURE',
+        subtitle: 'Innovate for Good',
+        content: 'Sustainability is at the core of our mission. We encourage projects and ideas that contribute to a sustainable future. Let’s work together to create solutions that benefit both people and the planet.',
+        button: 'Discover More',
+        src: Share,
+    }
+]
+
+export default mainContent;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,23 +1,75 @@
 import React from 'react';
 import styled from 'styled-components';
+import Button from '../assets/Button';
+import mainContent from '../assets/mainContent';
 
 const SectorContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-around;
-  gap: 10px;
+  gap: 20px;
   width: 90vw;
+  height: 600px;
   color: white;
   padding: 0 30px;
   margin: 0 auto;
+  border: 2px solid #668ba4;
+  border-radius: 20px;
+  margin-bottom: 20px;
+`;
+
+const SectorContentContainer = styled.div`
+  flex: 3;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const SectorTitle = styled.h2`
+  color: #668ba4;
+  font-size: 1em;
+`;
+
+const SectorSubTitle = styled.h2`
+  color: #2b2b91;
+  font-size: 2em;
+`;
+
+const SectorDescription = styled.p`
+  font-size: 1em;
+  text-align: left;
+`;
+
+const SectorImageContainer = styled.div`
+  flex: 2;
+`;
+
+const SectorImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
 
 const Main: React.FC = () => {
   return (
-    <SectorContainer role='sector'>
-      Main
-    </SectorContainer>
+    <div>
+      {mainContent.map((content, index) => (
+        <SectorContainer role='sector' key={index}>
+          <SectorContentContainer >
+            <SectorTitle>{content.title}</SectorTitle>
+            <SectorSubTitle>{content.subtitle}</SectorSubTitle>
+            <SectorDescription>{content.content}</SectorDescription>
+            <Button role='button'>{content.button}</Button>
+          </SectorContentContainer>
+          <SectorImageContainer>
+            <SectorImage src={content.src} alt={content.title} />
+        </SectorImageContainer>
+       </SectorContainer>
+      ))}
+    </div>
   )
 }
 
