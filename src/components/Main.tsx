62ccc2cc416d86c6fa4a741cdda38f8b958c1090
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-07-01 11:53:17 +0100

    Updated Main.tsx to pass test

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const SectorContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  gap: 10px;
+  width: 90vw;
+  color: white;
+  padding: 0 30px;
+  margin: 0 auto;
+`;
 
 const Main: React.FC = () => {
   return (
-    <div>Main</div>
+    <SectorContainer role='sector'>
+      Main
+    </SectorContainer>
   )
 }
 
