39ab60a7269f0e34cb7753b2ce178771461b0894
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-07-01 17:30:59 +0100

    Updated Main.test.tsx

--- src/tests/Main.test.tsx
+++ src/tests/Main.test.tsx
@@ -1,12 +1,59 @@
 import { render, screen } from '@testing-library/react';
-import { describe, it, expect} from 'vitest';
+import { describe, it, expect } from 'vitest';
 import Main from '../components/Main';
+import mainContent from '../assets/mainContent';
 
-describe('renders Main components correctly', () => {
-    it('renders without crashing', () => {
-      render(<Main />);
-      
-      const sectorContainer = screen.getByRole('sector');
-      expect(sectorContainer).toBeVisible();
+describe('Main component', () => {
+  it('renders without crashing', () => {
+    render(<Main />);
+    
+    const sectorContainer = screen.getAllByRole('sector');
+    expect(sectorContainer.length).toBe(mainContent.length);
+    sectorContainer.forEach(container => {
+      expect(container).toBeVisible();
     });
+  });
+
+  it('renders SectorTitle with correct text', () => {
+    render(<Main />);
+    mainContent.forEach(content => {
+      const sectorTitle = screen.getByText(content.title);
+      expect(sectorTitle).toBeInTheDocument();
+      expect(sectorTitle).toHaveStyle('color: #668ba4');
+    });
+  });
+
+  it('renders SectorSubTitle with correct text', () => {
+    render(<Main />);
+    mainContent.forEach(content => {
+      const sectorSubTitle = screen.getByText(content.subtitle);
+      expect(sectorSubTitle).toBeInTheDocument();
+      expect(sectorSubTitle).toHaveStyle('color: #2b2b91');
+    });
+  });
+
+  it('renders SectorDescription with correct text', () => {
+    render(<Main />);
+    mainContent.forEach(content => {
+      const sectorDescription = screen.getByText(content.content);
+      expect(sectorDescription).toBeInTheDocument();
+    });
+  });
+
+  it('renders Button with correct text', () => {
+    render(<Main />);
+    mainContent.forEach(content => {
+      const button = screen.getByRole('button', { name: content.button });
+      expect(button).toBeInTheDocument();
+    });
+  });
+
+  it('renders SectorImage with correct alt text', () => {
+    render(<Main />);
+    mainContent.forEach(content => {
+      const sectorImage = screen.getByAltText(content.title);
+      expect(sectorImage).toBeInTheDocument();
+      expect(sectorImage).toHaveAttribute('src', content.src);
+    });
+  });
 });
