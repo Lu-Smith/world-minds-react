deb64953336fc7ae47ef102545f3167da99a8d74
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-07-02 13:13:25 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import FooterHelpers from '../assets/FooterHelpers';
 
 const FooterText = styled.h4`
   font-size: 14px;
-  padding: 1.5em  0;
+  padding: 1.5em 0;
   color: #668ba4; 
   font-family: "Playwrite US Modern", cursive;
   width: 100vw;
@@ -17,6 +17,10 @@ const FooterText = styled.h4`
 
   @media (max-width: 768px) {
     font-size: 10px;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 9px;
   }
 `;
 
