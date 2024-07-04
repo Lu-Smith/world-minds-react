2c4b2d3f62bc36119b3540f9f09780e42bc24a7e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-07-04 23:04:17 +0100

    Added FooterStyles to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
+import { FooterText } from '../styles/FooterStyles';
 import SocialMedia from '../assets/SocialMedia';
 import FooterLink from '../assets/FooterLink';
 import FooterHelpers from '../assets/FooterHelpers';
-
-const FooterText = styled.h4`
-  font-size: 14px;
-  padding: 1.5em 0;
-  color: #668ba4; 
-  font-family: "Playwrite US Modern", cursive;
-  width: 100vw;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-
-  @media (max-width: 768px) {
-    font-size: 10px;
-  }
-
-  @media (max-width: 480px) {
-    font-size: 9px;
-  }
-`;
 
 const Footer: React.FC = () => {
   return (
