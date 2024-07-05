be08e247263e55755db23a79327c60a70438153a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-07-05 19:33:45 +0100

    Updated Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { SectorContainer, SectorContentContainer, SectorDescription, SectorImage, SectorImageContainer, SectorSubTitle, SectorTitle } from '../styles/MainStyles';
 import Button from '../assets/Button';
-import mainContent from '../assets/mainContent';
+import mainContent, { MainContentItem }  from '../assets/mainContent';
 import { motion } from "framer-motion";
 import ContactForm from '../assets/ContactForm';
 
 const Main: React.FC = () => {
   return (
     <>
-      {mainContent.map((content, index) => (
+      {mainContent.map((content: MainContentItem, index: number) => (
         <motion.div key={index} 
           initial={{opacity: 0, scale: 0.5}}
           whileInView={{opacity: 1, scale: 1}}
@@ -21,16 +21,16 @@ const Main: React.FC = () => {
           className={content.src ? '' : 'formContainer'}>
               <SectorContentContainer className={content.src ? '' : 'formContentContainer'}>
                 <SectorTitle>{content.title}</SectorTitle>
-                <SectorSubTitle 
-                className={content.src ? '' : 'subtitleForm'}
-                $reverse={index % 2 === 1 ? true : undefined}>
-                  {content.subtitle}
-                </SectorSubTitle>
-                <SectorDescription>{content.content}</SectorDescription>
-                {content.src ? 
-                <Button role='button'>{content.button}</Button> : 
-                ''
-                }
+                {content.subtitle && (
+                  <SectorSubTitle 
+                    className={content.src ? '' : 'subtitleForm'}
+                    $reverse={index % 2 === 1 ? true : undefined}
+                  >
+                    {content.subtitle}
+                  </SectorSubTitle>
+                )}
+                {content.content && <SectorDescription>{content.content}</SectorDescription>}
+                {content.button && <Button role='button'>{content.button}</Button>}
               </SectorContentContainer>
               {content.src ? 
               <SectorImageContainer>
